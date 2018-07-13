fd35f943fd90676dab787393c2363c5381fc8150
EmCue3 <41202065+EmCue3@users.noreply.github.com>
2018-07-13 12:24:52 -0400

    Update yearbook.py

--- yearbook.py
+++ yearbook.py
@@ -1,2 +1,3 @@
 #With great assistance from Simon Zeng
+#Alphabetizes a list of names by first initial of last name and first name
 print(', '.join(y[:y.index(' ') + 2] for y in sorted((x.strip().split(', ')[1] + ' ' + x.strip().split(', ')[0][0] for x in open('input1.txt', 'r').readlines()), key=lambda k: k.split()[1] + k.split()[0])))

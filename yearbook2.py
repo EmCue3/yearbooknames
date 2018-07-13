7dfeb6bd46367823d704b982d710763a341a4ec1
EmCue3 <41202065+EmCue3@users.noreply.github.com>
2018-07-13 12:23:05 -0400

    Update yearbook2.py

--- yearbook2.py
+++ yearbook2.py
@@ -1,2 +1,2 @@
 #With great assistance from Simon Zeng
-print(', '.join(sorted((x.strip().split(', ')[1] + ' ' + x.strip().split(', ')[0] for x in open('yes2.txt', 'r').readlines()), key=lambda k: k.split()[1] + k.split()[0])))
+print(', '.join(sorted((x.strip().split(', ')[1] + ' ' + x.strip().split(', ')[0] for x in open('input1.txt', 'r').readlines()), key=lambda k: k.split()[1] + k.split()[0])))
